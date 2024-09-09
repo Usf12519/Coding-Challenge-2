71bc94f048115805faf8367109dadb1b6a5400d8
Cameron Smith <Cs11@usf.edu>
2024-09-08 21:32:33 -0400

    Initial logic for calculating tip using ternary operator

---
+++ cc_2.js
@@ -0,0 +1,17 @@
+// Task 1: Calculating the Tip
+
+    if else( bill >= 50 && bill <= 500 ? * 0.15 :
+        bill * 0.20);
+
+    if else (bill >= 300{
+        tip = 0.2)
+        else{ 
+            tip =0.2);
+
+            return (tip * bill);
+
+
+        }
+    
+
+    
